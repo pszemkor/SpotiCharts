eb73db0e53cd216ec54b731949edaf551165e84b
Przemysław Jabłecki <prz.jab98@gmail.com>
2021-03-30 22:08:24 +0200

    Implement api for TOP200 and Viral50

--- spoticharts.py
+++ spoticharts.py
@@ -2,36 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from db import AudioFeatures, Viral50, Top200
 import pandas as pd
-from datetime import date
-
-
-class ViralService:
-    def __init__(self, engine):
-        self.engine = engine
-
-    def get_all(self, include_audio_features=True):
-        session = Session(bind=self.engine)
-        res = session.query(Viral50).all()
-        dicts = list(map(lambda item: item.__dict__, res))
-        if include_audio_features:
-            pass
-        df = pd.DataFrame(dicts)
-        df = df.drop(columns=['id', '_sa_instance_state'])
-        session.close()
-        return df
-
-    def get_all_by(self, country_code, include_audio_features=True, begin_date=None,
-                   end_date=None):
-        session = Session(bind=self.engine)
-        res = session.query(Viral50).filter(
-            Viral50.region == country_code and begin_date <= Viral50.date <= end_date).all()
-        dicts = list(map(lambda item: item.__dict__, res))
-        if include_audio_features:
-            pass
-        df = pd.DataFrame(dicts)
-        df = df.drop(columns=['id', '_sa_instance_state'])
-        session.close()
-        return df
 
 
 class SpotiCharts:
@@ -39,17 +9,45 @@ class SpotiCharts:
         engine = create_engine(db_string, echo=True)
         engine.connect()
         self.engine = engine
-        self.viral_service = ViralService(engine)
 
-    def viral(self):
-        return self.viral_service
+    def get_all_viral(self, country_codes=None, start_date=None, end_date=None, include_audio_features=True):
+        return self.__get_all_by_type(Viral50, country_codes, start_date, end_date, include_audio_features)
+
+    def get_all_top_200(self, country_codes=None, start_date=None, end_date=None, include_audio_features=True):
+        return self.__get_all_by_type(Top200, country_codes, start_date, end_date, include_audio_features)
 
     def get_audio_features(self):
-        pass
+        return self.__get_dataframe(AudioFeatures)
 
+    def __get_all_by_type(self, table, country_codes, start_date, end_date, include_audio_features):
+        session = Session(bind=self.engine)
+        predicates = []
+        if country_codes:
+            predicates.append(table.region.in_(country_codes))
+        if start_date:
+            predicates.append(start_date <= table.date)
+        if end_date:
+            predicates.append(table.date <= end_date)
+        res = session.query(table).filter(*predicates).all()
+        session.close()
 
-if __name__ == '__main__':
-    sc = SpotiCharts()
-    df = sc.viral().get_all()
-    print(df.head())
-    print(df.columns)
+        dicts = list(map(lambda item: item.__dict__, res))
+        if len(dicts) == 0:
+            raise Exception('No results matching the given criteria')
+
+        df = pd.DataFrame(dicts)
+        df.drop(columns=['id', '_sa_instance_state'], inplace=True)
+        if include_audio_features:
+            audio_df = self.get_audio_features()
+            audio_df.rename(columns={'id': 'spotify_id'}, inplace=True)
+            df = pd.merge(df, audio_df, on="spotify_id")
+        return df
+
+    def __get_dataframe(self, type):
+        session = Session(bind=self.engine)
+        res = session.query(type).all()
+        dicts = list(map(lambda item: item.__dict__, res))
+        df = pd.DataFrame(dicts)
+        df.drop(columns=['_sa_instance_state'], inplace=True)
+        session.close()
+        return df
